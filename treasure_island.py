d00e9899165045e8096e13cddb6f5f1960011353
EdemDevil <40803892+EdemDevil@users.noreply.github.com>
2023-01-16 17:17:24 +1000

    Update treasure_island.py

--- treasure_island.py
+++ treasure_island.py
@@ -1,17 +1,17 @@
 hello_msg = print("Добро пожаловать на остров сокровищ!\nВаша миссия найти сокровище.")
 
-choose_side = input("В какую сторону вы поплывёте? Налево или направо?	")
-if choose_side.lower() == "направо":
+choose_side = input("В какую сторону вы поплывёте? Налево или направо?	").lower()
+if choose_side == "направо":
     print("Игра окончена")
-elif choose_side.lower() == "налево":
-    choose_action = input("Плыть или ждать?	")
-    if choose_action.lower() == "плыть":
+elif choose_side) == "налево":
+    choose_action = input("Плыть или ждать?	").lower()
+    if choose_action == "плыть":
         print("Игра окончена")
-    elif choose_action.lower() == "ждать":
-        choose_door = input("Выберите дверь? Красная, синяя или желтая?	")
-        if choose_door.lower() == "синяя":
+    elif choose_action == "ждать":
+        choose_door = input("Выберите дверь? Красная, синяя или желтая?	").lower()
+        if choose_door == "синяя":
             print("Игра окончена")
-        elif choose_door.lower() == "красная":
+        elif choose_door == "красная":
             print("Игра окончена")
-        elif choose_door.lower() == "желтая":
+        elif choose_door == "желтая":
             print("Вы выйграли!")
